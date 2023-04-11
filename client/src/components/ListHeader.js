993bb060e5ed3cb99ba38782eeff2fbd0039d06a
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2023-04-10 23:16:03 -0700

    style list header

--- client/src/components/ListHeader.js
+++ client/src/components/ListHeader.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
+const signOut = () => {
+    console.log('sign out')
+}
+
 const ListHeader = ({ listName }) => {
     return (
-        <div>
-            {listName}
+        <div className='list-header'>
+            <h1>{listName}</h1>
+            <div className='button-container'>
+                <button className='create'>ADD NEW</button>
+                <button className='signout' onClick={signOut}>SIGN OUT</button>
+            </div>
         </div>
     )
 }

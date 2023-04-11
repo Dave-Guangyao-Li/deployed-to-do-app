ad732bd3d40eed9346f3399f93d816772e2fb040
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2023-04-11 16:33:38 -0700

    set up modal and its style

--- client/src/components/ListHeader.js
+++ client/src/components/ListHeader.js
@@ -1,17 +1,21 @@
-import React from 'react'
+import Modal from './Modal'
+import React, { useState } from 'react'
 
-const signOut = () => {
-    console.log('sign out')
-}
+
 
 const ListHeader = ({ listName }) => {
+    const [showModal, setShowModal] = useState(false) // to control if the modal is shown or not
+    const signOut = () => {
+        console.log('sign out')
+    }
     return (
         <div className='list-header'>
             <h1>{listName}</h1>
             <div className='button-container'>
-                <button className='create'>ADD NEW</button>
+                <button className='create' onClick={() => setShowModal(true)}>ADD NEW</button>
                 <button className='signout' onClick={signOut}>SIGN OUT</button>
             </div>
+            {showModal && <Modal mode={"create"} setShowModal={setShowModal} />}
         </div>
     )
 }
--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,8 +1,69 @@
 import React from 'react'
+import { useState } from 'react'
 
-const Modal = () => {
+const Modal = ({ mode, setShowModal }) => {
+    // const mode = "create"
+    const editMode = mode === "edit" ? true : false
+    const [data, setData] = useState({
+        user_email: "",
+        title: "",
+        progress: 0,
+        date: editMode ? "" : new Date()
+    })
+
+
+
+    const handleChange = (e) => {
+        console.log(e)
+        const { name, value } = e.target // this is destructuring, it's the same as const name = e.target.name and const value = e.target.value
+
+        // set key-value pairs in the data object
+        setData(data => ({
+            ...data,
+            [name]: value // overwrite the value of the key with the name of the input
+        }))
+
+        console.log(data)
+
+    }
+
     return (
-        <div>Modal</div>
+        <div className='overlay' >
+            <div className='modal'>
+                <div className='form-title-container'>
+                    <h3>Let's {mode} your task! </h3>
+                    <button onClick={() => setShowModal(false)}>X</button>
+                </div>
+
+                <form>
+                    <input
+                        required
+                        maxLength={30}
+                        placeholder=" Your task goes here"
+                        name="title"
+                        value={data.title}
+                        onChange={handleChange}
+                    />
+                    <br />
+                    <label for="range">Drag to select your current progress</label>
+                    <input
+                        required
+                        type="range"
+                        id="range"
+                        min="0"
+                        max="100"
+                        name="progress"
+                        value={data.progress}
+                        onChange={handleChange}
+                    />
+                    <input className={mode} type="submit" />
+                </form>
+
+
+
+
+            </div>
+        </div >
     )
 }
 
